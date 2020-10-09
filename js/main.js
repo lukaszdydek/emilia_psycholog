ea61c4fa3475bd3815f72e80078ac28aa585c07c
Łukasz Dydek <lukaszdydek@gmail.com>
2020-10-09 13:06:45 +0200

    mobile layout added with media queries

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*side-menu button action*/
+const menu_button = document.querySelector('.menu__button');
+const mobile_menu = document.querySelector('.mobile__menu');
+const mobile_contact = document.querySelector('.mobile__contact');
+
+menu_button.addEventListener('click', function() {
+    mobile_menu.classList.toggle('mobile__menu--active');
+    menu_button.classList.toggle('menu__button--clicked');
+    mobile_contact.classList.toggle('mobile__contact--active');
+}
+)
