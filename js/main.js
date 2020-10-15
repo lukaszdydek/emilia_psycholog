7a8e3b1b72b4afad56ced5ea5481f6f8cfabd1fb
Łukasz Dydek <lukaszdydek@gmail.com>
2020-10-16 00:23:19 +0200

    added About page, some links activated, CTA button turned into anchor element, JS added to modify buttons href attribute depending of windoww width

--- js/main.js
+++ js/main.js
@@ -12,24 +12,3 @@ menu_button.addEventListener('click', function() {
 }
 )
 
-/*collapsible containers*/
-
-const collapsible_button = document.querySelectorAll('.collapsible__button');
-const collapsible_content = document.querySelectorAll('.collapsible__content');
-
-for (i = 0; i < collapsible_button.length; i++) {
-    collapsible_button[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-        const content = this.nextElementSibling;
-        content.classList.toggle('collapsible__content--active');
-     
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-          } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-          }
-        }
-
-    )
-}
-
