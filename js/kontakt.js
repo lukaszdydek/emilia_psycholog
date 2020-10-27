4413e6308484c1fcb60a2fc678fce78a94891747
Łukasz Dydek <lukaszdydek@gmail.com>
2020-10-27 18:49:32 +0100

    fixed inconsistent image resolutions, added cookieless Google Maps support

--- js/kontakt.js
+++ js/kontakt.js
@@ -4,3 +4,19 @@ if (window.innerWidth < 768) {
     breaking.remove();
     
   }
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const rzelek = { lat: 50.033249, lng: 22.034834 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: rzelek,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: rzelek,
+    map: map,
+  });
+}
